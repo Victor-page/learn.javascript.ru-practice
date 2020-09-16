56d5256a1428f30bd6d8d908a1b2b9caa0fed3e5
Victor-official <vyk95@ukr.net>
2020-09-16 19:44:22 +0400

    new

---
+++ 37. Делегирование событий/scripts/Спрячьте сообщения с помощью делегирования.js
@@ -0,0 +1,11 @@
+//Дан список сообщений с кнопками для удаления [x]. Заставьте кнопки работать.
+
+container.addEventListener("click", handler);
+
+function handler(e) {
+  console.log(e.target);
+  if (e.target.className !== "remove-button") return;
+
+  const pane = e.target.closest(".pane");
+  pane.remove();
+}
